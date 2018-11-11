6d9fc6485dad722883e446e0c09e63b0fe6242e9
traderbook <contact@traderbook.net>
2018-11-11 10:09:50 +0100

    Revert "Issue #10 - Ajout au niveau de l'api de models"
    This reverts commit 6a7f442f8c59e2be623fba5002fc513b713f906f.

--- api/src/main/kotlin/com/traderbook/api/models/Instrument.kt
+++
@@ -1,9 +0,0 @@
-package com.traderbook.api.models
-
-import com.traderbook.api.enums.Instruments
-
-class Instrument(var name: Instruments, var ask: Double, var bid: Double, var oldAsk: Double, var oldBid: Double) {
-    override fun toString(): String {
-        return "Instrument(name=$name, oldAsk=$oldAsk, ask=$ask, oldBid=$oldBid, bid=$bid)"
-    }
-}
--- api/src/main/kotlin/com/traderbook/api/models/InstrumentCollection.kt
+++
@@ -1,3 +0,0 @@
-package com.traderbook.api.models
-
-class InstrumentCollection(val instruments: MutableMap<Long, Instrument>)
